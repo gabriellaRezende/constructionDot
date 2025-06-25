2ce9c0f9f53cf315b8263e40b1d0971e875f770c
Thales Hayashi <131497271+TamagoGamer@users.noreply.github.com>
2025-06-25 22:11:26 +0100

    fix: :ambulance: Produtos Aparecem no Index (Manualmente)

--- pbe/settings.py
+++ pbe/settings.py
@@ -110,7 +110,8 @@ AUTHENTICATION_BACKENDS = (
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
     },
 }
 
